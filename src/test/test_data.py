ce69558d8aab03366c0f1272a9d45fb3be498663
Leah Nguyen <nduongthucanh@gmail.com>
2021-10-30 12:11:57 +0700

    Update test_data.py

--- src/test/test_data.py
+++ src/test/test_data.py
@@ -1,1 +1,11 @@
-# To be filled by students
+import unittest
+import pandas as pd
+import numpy as np 
+from data import Dataset
+
+
+class TestDataset(unittest.TestCase):
+    def test_get_name(self):
+        
+if __name__ == '__main__':
+    unittest.main()
